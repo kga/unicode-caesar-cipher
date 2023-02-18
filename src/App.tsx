d69165100bbda8eeb75d90a678fcb5cde4bd21da
Wataru TOYA <watrty@gmail.com>
2023-02-18 20:50:58 +0900

    code point でシーザー暗号する君

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const q = "徳川綱吉は江戸幕府第何代将軍？";
+
+  const encrypt = (shiftNum: number, question: string): string => Array.from(question)
+    .map((c) => String.fromCodePoint(c.charCodeAt(0) + shiftNum))
+    .join("");
+
+  const [shiftNum, setShiftNum] = useState(1);
+  const [question, setQuestion] = useState(q);
+  const [encryptedQuestion, setEncryptedQuestion] = useState(
+    encrypt(shiftNum, question)
+  );
+
+  const handleShiftNumChange = (shiftNum: number) => {
+    setEncryptedQuestion(encrypt(shiftNum, question));
+    return shiftNum;
+  };
+
+  const handleQuestionChange = (question: string) => {
+    setEncryptedQuestion(encrypt(shiftNum, question));
+    return question;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>Unicode シーザー暗号化</h1>
       </header>
-    </div>
+      <main>
+        <form>
+          <input
+            type="number"
+            onChange={(e) => {
+              setShiftNum(handleShiftNumChange(Number(e.target.value)));
+            }}
+            value={shiftNum}
+          />
+          <input
+            type="text"
+            onChange={(e) => {
+              setQuestion(handleQuestionChange(e.target.value));
+            }}
+            value={question}
+            size={30}
+          />
+          <input
+            type="text"
+            value={encryptedQuestion}
+            size={30}
+            readOnly={true}
+          />
+        </form>
+      </main>
+      <footer>
+        <a href="https://youtu.be/h9xUvzJdRrA">https://youtu.be/h9xUvzJdRrA</a>
+      </footer>
+    </>
   );
 }
 
