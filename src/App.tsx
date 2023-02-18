b3c0f0136dcf09ab4912f1a97b112621176ed102
Wataru TOYA <watrty@gmail.com>
2023-02-18 21:06:58 +0900

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import "./App.css";
+import { useState } from "react";
 
 function App() {
   const q = "徳川綱吉は江戸幕府第何代将軍？";
 
-  const encrypt = (shiftNum: number, question: string): string => Array.from(question)
-    .map((c) => String.fromCodePoint(c.charCodeAt(0) + shiftNum))
-    .join("");
+  const encrypt = (shiftNum: number, question: string): string =>
+    Array.from(question)
+      .map((c) => String.fromCodePoint(c.charCodeAt(0) + shiftNum))
+      .join("");
 
   const [shiftNum, setShiftNum] = useState(1);
   const [question, setQuestion] = useState(q);
@@ -26,37 +26,22 @@ function App() {
 
   return (
     <>
-      <header>
-        <h1>Unicode シーザー暗号化</h1>
-      </header>
-      <main>
-        <form>
-          <input
-            type="number"
-            onChange={(e) => {
-              setShiftNum(handleShiftNumChange(Number(e.target.value)));
-            }}
-            value={shiftNum}
-          />
-          <input
-            type="text"
-            onChange={(e) => {
-              setQuestion(handleQuestionChange(e.target.value));
-            }}
-            value={question}
-            size={30}
-          />
-          <input
-            type="text"
-            value={encryptedQuestion}
-            size={30}
-            readOnly={true}
-          />
-        </form>
-      </main>
-      <footer>
-        <a href="https://youtu.be/h9xUvzJdRrA">https://youtu.be/h9xUvzJdRrA</a>
-      </footer>
+      <input
+        type="number"
+        onChange={(e) => {
+          setShiftNum(handleShiftNumChange(Number(e.target.value)));
+        }}
+        value={shiftNum}
+      />
+      <input
+        type="text"
+        onChange={(e) => {
+          setQuestion(handleQuestionChange(e.target.value));
+        }}
+        value={question}
+        size={30}
+      />
+      <input type="text" value={encryptedQuestion} size={30} readOnly={true} />
     </>
   );
 }
