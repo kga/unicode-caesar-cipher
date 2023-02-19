4aabef67177d44a16099bedea6edf68493d8100c
Wataru TOYA <watrty@gmail.com>
2023-02-19 22:41:38 +0900

    component test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders default question", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const shift = screen.getByDisplayValue<HTMLInputElement>(1);
+  const question =
+    screen.getByDisplayValue<HTMLInputElement>(
+      "徳川綱吉は江戸幕府第何代将軍？"
+    );
+  const encrypted =
+    screen.getByDisplayValue<HTMLInputElement>(
+      "徴州網吊ば池戹幖庝笭佖令將軎＠"
+    );
+
+  fireEvent.change(question, {
+    target: { value: "『高瀬舟』や『舞姫』を書いた小説家は？" },
+  });
+  expect(encrypted.value).toBe("』髙瀭舠【ゅ』舟姬【ん曹ぅだ尐読宷ば＠");
+
+  fireEvent.change(shift, {
+    target: { value: "2" },
+  });
+  fireEvent.change(question, {
+    target: { value: "ちちゅうかいせいきこう" },
+  });
+  expect(encrypted.value).toBe("っっょえきうそうくさえ");
 });
